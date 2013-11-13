5455aceb4d467ac45fe220c603ebd6fa5ba48f86
user <user@domain>
2013-11-13 22:02:05 +0000

    with connect

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(80);
-console.log('Server running at http://127.0.0.1:80/');
+var connect = require('connect');
+connect.createServer(
+    connect.static(__dirname)
+).listen(80);
